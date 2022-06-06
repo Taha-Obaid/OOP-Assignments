da351a926c0239174364ea11065e2279a22ff350
M.Taha Obaid <105553138+Taha-Obaid@users.noreply.github.com>
2022-06-06 12:10:40 +0500

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Addition
+{
+  void add(int a , int b)
+   {
+    int x = a;
+    int y = b;
+    System.out.println("x + y =  " + (x+y));
+   }
+  void add(int a , int b , int c)
+   {
+    int x = a;
+    int y = b;
+    int z = c;
+    System.out.println("x + y + z =  " + (x + y + z));
+   }
+}
+
+public class MethodOverloading
+{
+  public static void main(String args[])
+   {
+      Scanner S = new Scanner(System.in);
+     Addition P = new Addition();
+    System.out.println("\nEnter values of a , b & c");
+     int a = S.nextInt();
+     int b = S.nextInt();
+     int c = S.nextInt();
+      P.add( a , b );
+      P.add( a , b , c );
+   }
+}
